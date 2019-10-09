0372d94cf8da5eaa149f5c758a752e4813a570f1
TroubnikovaM <trubnikova.mp@gmail.com>
2019-10-09 09:45:24 +0000

    change css file

--- fl.py
+++ fl.py
@@ -2,10 +2,10 @@ from flask import render_template
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/login')
-@app.route('/login/<name>')
-def login(name=None):
-    return render_template('login.html', name=name)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    return render_template('login.html')
 
 if __name__ == '__main__':
 	app.debug = True
