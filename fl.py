3ebff318c1fa508b292e0c38a6ad7487b7f5c5c0
MaryFeles <trubnikova.mp@gmail.com>
2019-10-16 09:43:35 +0000

    add dashboard.html, main.html

--- fl.py
+++ fl.py
@@ -1,12 +1,34 @@
-from flask import render_template
+from flask import render_template, request, url_for
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    
-    return render_template('login.html')
+@app.route('/')
+def homepage():
+    return render_template("main.html")
 
+@app.route('/dashboard/')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/login/', methods=["GET","POST"])
+def login_page():
+    error = ''
+    try:
+        if request.method == "POST":
+            attempted_username = request.form['username']
+            attempted_password = request.form['password']
+            
+            if attempted_username == "admin" and attempted_password == "password":
+                return redirect(url_for('dashboard'))
+            else:
+                error = "Try Again"
+                
+        return render_template("login.html", error = error)
+    
+    except Exception as e:
+        
+        return render_template("login.html", error = error)
+            
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
