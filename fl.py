584c1a15a0cc505aa58d4537e61a218621c19e1a
MaryFeles <trubnikova.mp@gmail.com>
2019-10-21 13:16:31 +0000

    добавила   mydatabase.db test.py

--- fl.py
+++ fl.py
@@ -1,5 +1,10 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, url_for, redirect
 from flask import Flask
+import sqlite3
+
+conn = sqlite3.connect("test.db", check_same_thread=False)
+cursor = conn.cursor()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,22 +17,26 @@ def dashboard():
 
 @app.route('/login/', methods=["GET","POST"])
 def login_page():
-    error = ''
-    try:
+        error = ''
+    #try:
         if request.method == "POST":
             attempted_username = request.form['username']
             attempted_password = request.form['password']
+            a = "select * from users where login = '%s' and pass = '%s';" % (attempted_username, attempted_password)
+            cursor.execute(a)
             
-            if attempted_username == "admin" and attempted_password == "password":
-                return redirect(url_for('dashboard'))
-            else:
-                error = "Try Again"
+            return render_template("login.html", error = cursor.fetchall())
+            
+           # if attempted_username == "admin" and attempted_password == "password":
+            #    return redirect(url_for('dashboard'))
+          #  else:
+          #      error = "Try Again"
                 
         return render_template("login.html", error = error)
     
-    except Exception as e:
+    #except Exception as e:
         
-        return render_template("login.html", error = error)
+      #  return render_template("login.html", error = e)
             
 if __name__ == '__main__':
 	app.debug = True
